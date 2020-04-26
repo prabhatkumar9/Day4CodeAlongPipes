b80361e9a3fac2868904285a78e65cdd3992453e
prabhatkumar9 <prabhatvisvakarma9@gmail.com>
2020-04-26 08:06:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPipes';
+
+
+  blr: string = '';
+  fcs: string = '';
+  scl: string = '';
+  name: string = '';
+  number: number = 0;
+  currentDate: number = 0;
+
+  clickCounter: number = 0;
+  dblclickCounter: number = 0;
+
+  click(): void {
+    this.clickCounter += 1;
+  }
+
+  dblclick(): void {
+    this.dblclickCounter += 1;
+  }
+
+
+  blur(): void {
+    console.log("Blur event is working.");
+    this.blr = "Blur event is working.";
+  }
+
+  focus(): void {
+    console.log("Focus event is working.");
+    this.fcs = "Focus event is working.";
+  }
+
+  scroll(): void {
+    console.log("Scroll is working.");
+    this.scl = "Scroll is working.";
+  }
+
+  pipes(names): void {
+    this.name = names;
+  }
+
+  Number(currency: number): void {
+    this.number = currency;
+  }
+
+  date(): void {
+    this.currentDate = Date.now();
+  }
 }
